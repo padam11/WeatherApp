c8d05b675411a519409f5aaeeb5d0a21f8acdbc4
Rohan Buch <buchrohand2@gmail.com>
2023-07-03 23:30:14 -0700

    Update WeatherApi.js

--- js/WeatherApi.js
+++ js/WeatherApi.js
@@ -1,15 +1,17 @@
-class Weather {
-    constructor() {
-      this.apiKeys = '8ab050093f4fbd71760ac840b7660a0c';
-    }
-    async retrieveWeather(city)
-    {
-      const response = await fetch('https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${this.apiKeys}');
-      const res = await response.json();
-      return
-      {
-        res
-      }
-      
+export class Weather {
+  constructor() {
+    this.apiKeys = '8ab050093f4fbd71760ac840b7660a0c';
+  }
+  async retrieveWeather(city)
+  {
+    const response = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${this.apiKeys}`);
+    
+    const resData = await response.json();
+    
+    return {
+      resData
     }
   }
+  
+  
+}
